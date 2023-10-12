40eb4b81163e6a365b1c80f650b80fda50e61644
bobauser <111920859+bobauser@users.noreply.github.com>
2023-10-12 19:21:41 +0200

    Create page.js

---
+++ src/app/page.js
@@ -0,0 +1,13 @@
+"use client";
+
+import React, { useState } from 'react';
+import questionsData from '../lib/alternativ1.json';
+import Wow from '../components/Wow';
+
+export default function Home() {
+  return (
+    <div>
+      <Wow />
+    </div>
+  );
+}
